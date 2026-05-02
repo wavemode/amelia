07de1968e1d0a7053a8a549e91587a16bbbd0ad1
Chukwudi Okechukwu <w4vem0de@gmail.com>
2026-05-02 10:01:07 -0500

    Option

---
+++ src/data/util/option.h
@@ -0,0 +1,162 @@
+#pragma once
+
+#include <utility>
+
+namespace amelia {
+
+class RuntimeError;
+
+struct None {};
+
+template <typename T> struct Some {
+  T value;
+  Some(T value) : value(std::move(value)) {}
+};
+
+template <typename T> class Option {
+public:
+  Option() {}
+
+  Option(T value) { initialize(std::move(value)); }
+
+  Option(Some<T> some) { initialize(std::move(some.value)); }
+
+  Option(None) {}
+
+  template <typename U, typename = std::enable_if_t<std::is_convertible_v<U, T>>>
+  Option(U &&value) {
+    initialize(static_cast<T>(std::forward<U>(value)));
+  }
+
+  template <typename U, typename = std::enable_if_t<std::is_convertible_v<U, T>>>
+  Option(Some<U> &&some) {
+    initialize(static_cast<T>(std::forward<U>(some.value)));
+  }
+
+  Option(const Option<T> &other) {
+    if (other.m_has_value) {
+      initialize(*other.get());
+    }
+  }
+
+  Option(Option<T> &&other) {
+    if (other.m_has_value) {
+      initialize(std::move(*other.get()));
+    }
+    other.m_has_value = false;
+  }
+
+  Option<T> &operator=(const Option<T> &other) {
+    if (this != &other) {
+      if (other.m_has_value) {
+        assign(*other.get());
+      } else {
+        destroy();
+      }
+    }
+    return *this;
+  }
+
+  Option<T> &operator=(Option<T> &&other) {
+    if (this != &other) {
+      if (other.m_has_value) {
+        assign(std::move(*other.get()));
+      } else {
+        destroy();
+      }
+      other.m_has_value = false;
+    }
+    return *this;
+  }
+
+  template <typename U, typename = std::enable_if_t<std::is_convertible_v<U, T>>>
+  Option<T> &operator=(U &&value) {
+    assign(static_cast<T>(std::forward<U>(value)));
+    return *this;
+  }
+
+  template <typename U, typename = std::enable_if_t<std::is_convertible_v<U, T>>>
+  Option<T> &operator=(Some<U> &&some) {
+    assign(static_cast<T>(std::forward<U>(some.value)));
+    return *this;
+  }
+
+  ~Option() { destroy(); }
+
+  bool has_value() const noexcept { return m_has_value; }
+
+  T &value() {
+    if (!m_has_value) {
+      throw RuntimeError("Attempted to access value of an empty Option");
+    }
+    return *get();
+  }
+
+  const T &value() const {
+    if (!m_has_value) {
+      throw RuntimeError("Attempted to access value of an empty Option");
+    }
+    return *get();
+  }
+
+  T &operator*() { return value(); }
+
+  T *operator->() {
+    if (!m_has_value) {
+      throw RuntimeError("Attempted to access value of an empty Option");
+    }
+    return get();
+  }
+
+  void clear() { destroy(); }
+
+  static Option<T> some(T value) { return Option(std::move(value)); }
+  static Option<T> none() { return Option(); }
+
+  bool operator==(const Option<T> &other) const {
+    if (m_has_value != other.m_has_value) {
+      return false;
+    }
+    if (m_has_value) {
+      return *get() == *other.get();
+    }
+    return false;
+  }
+
+  bool operator!=(const Option<T> &other) const { return !(*this == other); }
+
+private:
+  union Container {
+    T value;
+    char storage[sizeof(T)];
+    ~Container() {}
+  };
+
+  void initialize(T value) {
+    new (get()) T(std::move(value));
+    m_has_value = true;
+  }
+
+  void assign(T value) {
+    if (m_has_value) {
+      *get() = std::move(value);
+    } else {
+      initialize(std::move(value));
+    }
+  }
+
+  void destroy() {
+    if (m_has_value) {
+      m_has_value = false;
+      get()->~T();
+    }
+  }
+
+  T *get() { return reinterpret_cast<T *>(&m_container.storage); }
+  const T *get() const { return reinterpret_cast<const T *>(&m_container.storage); }
+
+  bool m_has_value = false;
+  Container m_container{.storage = {}};
+};
+
+} // namespace amelia
---
+++ src/test/data/util/option_test.cpp
@@ -0,0 +1,45 @@
+#include <doctest.h>
+
+#include "prelude.h"
+
+#include "data/util/option.h"
+
+TEST_SUITE_BEGIN("Option");
+
+using namespace amelia;
+
+TEST_CASE("general usage") {
+  Option<String> opt;
+  CHECK(!opt.has_value());
+
+  opt = "Hello";
+  CHECK(opt.has_value());
+  CHECK(opt.value() == "Hello");
+  CHECK(opt == "Hello");
+  CHECK(opt != "World");
+
+  opt.value() = "World";
+  CHECK(opt.value() == "World");
+  CHECK((*opt).begin().peek() == 'W');
+  CHECK(opt->size() == 5);
+  opt->append("!");
+  CHECK(opt->text() == "World!");
+
+  opt = None();
+  CHECK(!opt.has_value());
+  CHECK_THROWS_AS(opt.value(), RuntimeError);
+  CHECK_THROWS_AS(*opt, RuntimeError);
+
+  Option<int> opt2 = Some(10);
+  CHECK(opt2.has_value());
+  CHECK(opt2.value() == 10);
+  CHECK(opt2 == 10);
+  CHECK(opt2 != 20);
+  CHECK(opt2 == Some(10));
+  CHECK(opt2 != Some(20));
+  opt2 = 20;
+  CHECK(opt2.value() == 20);
+  opt2 = Some(30);
+  CHECK(*opt2 == 30);
+}
+TEST_SUITE_END();
