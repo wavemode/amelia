ef02d0512c0a0145287ef32939ba68f7622b8ef8
Chukwudi Okechukwu <w4vem0de@gmail.com>
2026-07-21 12:19:14 -0500

    don't serialize Module name

--- src/sema/data/module.cpp
+++ src/sema/data/module.cpp
@@ -8,7 +8,6 @@ namespace amelia {
 Serialize Module::serialize() const {
   auto result = Serialize();
   result.set_object_name("Module");
-  result.add_object_field("name", Serialize::quoted(name));
   result.add_object_field("scope", scope->serialize());
   return result;
 }
